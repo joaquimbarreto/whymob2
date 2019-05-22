9771bb8294866c7c83fd7546b10db25113bb07ba
joaquimbarreto <joaquim@barreto.co>
2019-05-22 17:26:22 +0100

    All fetch ok

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 const cloudListEl = document.querySelector("#cloud-list");
 const systemUserEl = document.querySelector("#system-user");
-const detaislEl = document.querySelector("#details");
+const detailsEl = document.querySelector("#details");
 let accountId = 1;
 
 // add a single cloud to the page
 const addCloud = cloud => {
 	const cloudItem = document.createElement("li");
-	//   const agentButton = document.createElement("button");
 	cloudItem.className = "cloud-item";
 	cloudItem.id = cloud.id;
-	cloudItem.innerHTML = `
-    <img src="${cloud.CloudLogo}" />
-  `;
-
+	cloudItem.innerHTML = `<img src="${cloud.CloudLogo}" />`;
 	cloudListEl.appendChild(cloudItem);
 };
 
 // use the above function multiple times
 const addClouds = clouds => {
-	// debugger;
 	clouds.result.forEach(cloud => addCloud(cloud));
 };
 
 const addSystemUser = user => {
 	const systemUserImg = document.createElement("img");
-	systemUserImg.src = user.CloudLogo;
+	systemUserImg.className = "main-image";
+	systemUserImg.src = user.result[0].Photo;
 	systemUserEl.appendChild(systemUserImg);
+};
+
+const addAccountDetails = account => {
+	const accountItem = document.createElement("div");
+	accountItem.innerHTML = `
+		<p><strong>ID</strong>: ${account.result[0].Id} </p>
+		<p><strong>Name:</strong> ${account.result[0].Name} </p>
+		<p><strong>Description:</strong> ${account.result[0].Description} </p>
+		<p><strong>Date Created:</strong> ${account.result[0].CreateDate} </p>
+	`;
+	detailsEl.appendChild(accountItem);
 };
 
 // GET all Service from the server
@@ -49,3 +56,4 @@ const getAccount = () =>
 // GET all the clouds, *then* put them on the page upon load
 getClouds().then(addClouds);
 getSystemUser().then(addSystemUser);
+getAccount().then(addAccountDetails);
